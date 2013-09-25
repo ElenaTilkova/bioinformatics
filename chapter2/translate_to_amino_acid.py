473c17474d485577eddfcc8e0d28388d5f0a780e
Sergey Koklyuev <sergey.kokljuev@gmail.com>
2013-09-25 23:29:09 +0400

    begin implement translator from rna string to amino acid string

---
+++ chapter2/translate_to_amino_acid.py
@@ -0,0 +1,8 @@
+
+string = ''
+
+translator = { 'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q', 'CCU': 'P',
+               'CCC': 'P', 'CCA': 'P', 'CCG': 'P', 'CGU': 'R', 'CGC': 'R',
+               'CGA': 'R', 'CGG': 'R', 'CUU': 'L', 'CUC': 'L', 'CUA': 'L',
+               'CUG': 'L', 'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+               'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A', 
