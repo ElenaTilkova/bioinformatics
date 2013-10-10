dce9255784710c12f9bb5cce3c3155214b0d1158
Sergey Koklyuev <sergey.kokljuev@gmail.com>
2013-10-10 14:51:26 +0400

    add finding cyclospectrum of peptide

--- chapter2/cyclospectrum.py
+++ chapter2/cyclospectrum.py
@@ -1,33 +1,60 @@
 
-peptide = "QWERB"
-difference_amino_acid = set()
+peptide_mass = dict()
+peptide_mass['G'] = 57
+peptide_mass['A'] = 71
+peptide_mass['S'] = 87
+peptide_mass['P'] = 97
+peptide_mass['V'] = 99
+peptide_mass['T'] = 101
+peptide_mass['C'] = 103
+peptide_mass['I'] = 113
+peptide_mass['L'] = 113
+peptide_mass['N'] = 114
+peptide_mass['D'] = 115
+peptide_mass['K'] = 128
+peptide_mass['Q'] = 128
+peptide_mass['E'] = 129
+peptide_mass['M'] = 131
+peptide_mass['H'] = 137
+peptide_mass['F'] = 147
+peptide_mass['R'] = 156
+peptide_mass['Y'] = 163
+peptide_mass['W'] = 186
 
-for letter in peptide:
-    difference_amino_acid.add(letter)
+peptide = "IRGWEWVPFLCVT"
 
-letters = []
-for letter in difference_amino_acid:
-    letters.append(letter)
+def get_slices(peptide, k):
+    result = []
+    if k == len(peptide):
+        result.append(peptide)
+    else:
+        for i in range(len(peptide)):
+            tmp = peptide[i:min(len(peptide), i + k)] + peptide[0:max(0, i + k - len(peptide))]
+            result.append(tmp)
+    if k > 1:
+        tmp = get_slices(peptide, k - 1)
+        for item in tmp:
+            result.append(item)
+    return result
 
-def combinate(peptides, letters):
-    tmp = []
-    results = []
-    for i in peptides:
-        for j in letters:
-            if not j in i:
-                tmp.append(i + j)
-    if len(tmp[0]) < len(letters):
-         results = combinate(tmp, letters)
-    for i in peptides:
-        results.append(i)
-    return results
+def get_mass(item):
+    mass = 0
+    for letter in item:
+        mass = mass + peptide_mass[letter]
+    return mass
 
-tmp = combinate(letters, letters)
-result = set()
+slices = get_slices(peptide[::-1], len(peptide))
 
-for i in tmp:
-    result.add(''.join(sorted(i)))
+given_masses = []
+for item in slices:
+    given_masses.append(get_mass(item))
+given_masses.append(0)
+given_masses = sorted(given_masses)
 
+str_given_masses = []
+for item in given_masses:
+    str_given_masses.append(str(item))
 
-
-
+f = open('C:/projects/test.txt', 'w')
+print(' '.join(str_given_masses), file=f)
+f.close()
